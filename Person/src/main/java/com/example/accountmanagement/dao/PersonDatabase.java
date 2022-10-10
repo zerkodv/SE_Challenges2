7c3c270d3fa21f53b48687754098547dfce62c1e
zerkodv <s3937120@student.rmit.edu.au>
2022-10-10 16:58:48 +1100

    added repository

--- Person/src/main/java/com/example/accountmanagement/dao/PersonDatabase.java
+++ Person/src/main/java/com/example/accountmanagement/dao/PersonDatabase.java
@@ -2,6 +2,8 @@ package com.example.accountmanagement.dao;
 
 import com.example.accountmanagement.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PersonDatabase extends JpaRepository<Person, Long> {
 }
--- Person/src/main/java/com/example/accountmanagement/model/Person.java
+++ Person/src/main/java/com/example/accountmanagement/model/Person.java
@@ -1,7 +1,15 @@
 package com.example.accountmanagement.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Person {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
     private String address;
@@ -22,7 +30,9 @@ public class Person {
         this.phoneno = phoneno;
     }
 
+    public Person() {
 
+    }
 
 
     public Long getId() {
